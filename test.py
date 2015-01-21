41f99ac2d21bd780da520ff38f41f550bc89d8a2
daniyuu <daniyuu@163.com>
2015-01-21 20:55:14 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      daniyuu
+#
+# Created:     21/01/2015
+# Copyright:   (c) daniyuu 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import pygame
+
+def main():
+    if pygame.font is None:
+        print "The font module is not available!"
+    exit()
+
+
+if __name__ == '__main__':
+    main()
